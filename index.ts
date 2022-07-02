e0366a3584be3db6276480d1a8cdc888186ccdc2
Nicholas Hamilton <nickh3592@gmail.com>
2022-07-01 23:12:01 -0400

    adds request counter

--- index.ts
+++ index.ts
@@ -14,27 +14,27 @@ interface fetchedDataArray {
     name: string,
     first_appeared: string,
     price: number,
-    last_appeared: string | number | null 
+    last_appeared: string | number | null
 }
 
-
+// https://web-api.coinmarketcap.com/v1/cryptocurrency/listings/historical?&date=2022-05-01&limit=5000&start=20000
 
 const main = async () => {
     console.time('fetch')
     let date: string | number = new Date("2013-04-29").toLocaleDateString('en-CA')
-    const stopDate = new Date().getTime()
-    const url = `https://web-api.coinmarketcap.com/v1/cryptocurrency/listings/historical?&date=${date}`
+    const stopDate = new Date("2014-04-29").getTime()
+    const url = `https://web-api.coinmarketcap.com/v1/cryptocurrency/listings/historical?&date=${date}&limit=5000`
 
 
     const data: Array<fetchedDataArray> = []
+    let counter = 0
 
     while (new Date(date).getTime() <= stopDate) {
-        // sleep for 5 seconds before making the next api request
-        await new Promise(resolve => setTimeout(resolve, 5000)) 
+        // sleep for 1 seconds before making the next api request
+        await new Promise(resolve => setTimeout(resolve, 1000))
         try {
-
             const response = await axios.get(url)
-            response.data.data.map((i:fetchedData) => {
+            response.data.data.map((i: fetchedData) => {
                 const res = {
                     name: i.name,
                     first_appeared: i.date_added,
@@ -43,22 +43,24 @@ const main = async () => {
                 }
 
                 //if the coin is not in the response, it means it was removed from the market, so we want to update the last_appeared in the data array
-                if (!response.data.data.find((j: fetchedData )=> j.name === i.name)) {
+                if (!response.data.data.find((j: fetchedData) => j.name === i.name)) {
                     console.log(`${i.name} was removed from the market`)
                     const index = data.findIndex(j => j.name === i.name)
                     data[index].last_appeared = date
                 }
 
 
-
-                data.push(res)                
+                console.log(res)
+                data.push(res)
             })
 
         } catch (err) {
             console.log(err)
         }
-        
 
+
+        counter++
+        console.log(`${counter} requests made`)
         date = new Date(date).setDate(new Date(date).getDate() + 7);
         date = new Date(date).toLocaleDateString('en-CA')
     }
@@ -66,8 +68,8 @@ const main = async () => {
     console.log(data.length)
 
     // finds all counts that have last_appeared not null
-    const count = data.filter((i:fetchedDataArray) => i.last_appeared !== null).length
-    console.log('number of none null last appeared value',count)
+    const count = data.filter((i: fetchedDataArray) => i.last_appeared !== null).length
+    console.log('number of none null last appeared value', count)
     console.timeEnd('fetch')
 
 
